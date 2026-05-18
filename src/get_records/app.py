7466fddc9f3093f73d02b5e99fef8d083920a024
Honeyman-80 <david.w.honeyman@gmail.com>
2026-05-18 08:23:25 -0600

    Implement DynamoDB scan in lambda_handler

--- src/get_records/app.py
+++ src/get_records/app.py
@@ -1,1 +1,20 @@
+import json
+import os
+import boto3
 
+dynamodb = boto3.resource("dynamodb")
+table_name = os.environ["TABLE_NAME"]
+table = dynamodb.Table(table_name)
+
+def lambda_handler(event, context):
+    result = table.scan()
+
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "records": result.get("Items", [])
+        })
+    }
