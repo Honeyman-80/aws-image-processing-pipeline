fbb92480592185403eb21f4a1d09e15d3beae1e6
Honeyman-80 <david.w.honeyman@gmail.com>
2026-05-21 17:00:43 -0600

    Integrate DynamoDB for upload URL tracking
    Add DynamoDB integration to store upload details.

--- src/get_upload_url/app.py
+++ src/get_upload_url/app.py
@@ -2,10 +2,15 @@ import json
 import uuid
 import boto3
 import os
+from datetime import datetime, timezone
 
 s3 = boto3.client("s3")
 
+dynamodb = boto3.resource("dynamodb")
+
 BUCKET_NAME = os.environ["BUCKET_NAME"]
+TABLE_NAME = os.environ["TABLE_NAME"]
+table = dynamodb.Table(TABLE_NAME)
 
 def lambda_handler(event, context):
 
@@ -14,6 +19,16 @@ def lambda_handler(event, context):
     image_id = str(uuid.uuid4())
 
     object_key = f"uploads/{user_id}/{image_id}.jpg"
+
+    table.put_item(
+        Item={
+            "user_id": user_id,
+            "image_id": image_id,
+            "status": "UPLOAD_URL_CREATED",
+            "object_key": object_key,
+            "created_at": datetime.now(timezone.utc).isoformat()
+        }
+    )
 
     upload_url = s3.generate_presigned_url(
         "put_object",
